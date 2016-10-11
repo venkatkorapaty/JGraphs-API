3f76e103bacb1f1ed8e756b7d588fac92e797af3
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2016-10-11 10:56:02 -0400

    added a few JUnit templates

---
+++ JGraphs/src/test/Graphs/DirectedGraphTest.java
@@ -0,0 +1,24 @@
+package test.Graphs;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DirectedGraphTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
