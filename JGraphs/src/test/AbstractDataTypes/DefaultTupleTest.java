4ed23a6360ddbbf3e920466ce6caa7488c9cedf5
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2017-01-01 00:57:08 -0500

    fixed bugs and wrote more test cases

--- JGraphs/src/test/AbstractDataTypes/DefaultTupleTest.java
+++ JGraphs/src/test/AbstractDataTypes/DefaultTupleTest.java
@@ -5,11 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import main.AbstractDataTypes.*;
+import main.AbstractDataTypesInterfaces.*;
+import main.Vertices.*;
+import java.util.*;
 
 public class DefaultTupleTest {
 
+	Tuple t1;
+	Tuple t2;
+	Vertex v1;
+	Vertex v2;
+	Vertex v3;
+	Map<Tuple, Boolean> tuples;
+	
 	@Before
 	public void setUp() throws Exception {
+		v1 = new UndirectedVertex(1, "test");
+		v2 = new UndirectedVertex(2, "test1");
+		v3 = new UndirectedVertex(1, "test");
+		t1 = new DefaultTuple(v1, v2);
+		t2 = new DefaultTuple(v2, v3);
+		tuples = new HashMap<Tuple, Boolean>();
 	}
 
 	@After
@@ -17,8 +34,20 @@ public class DefaultTupleTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		assertEquals(t1, t2);
 	}
 
+	@Test
+	public void testTupleMap() {
+		Tuple t3 = new DefaultTuple(v2, v1);
+		tuples.put(t1, new Boolean(true));
+		assertTrue(tuples.get(t3));
+	}
+
+	@Test
+	public void testSwitchedTuple() {
+		tuples.put(t1, new Boolean(true));
+		assertTrue(tuples.get(t2));
+	}
 }
