9852b13103438546b9759eaacad01844e7253864
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2017-04-30 14:24:44 -0400

    added test file for DirectedVertex

---
+++ JGraphs/src/test/Vertices/DirectedVertexTest.java
@@ -0,0 +1,33 @@
+package test.Vertices;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import main.Vertices.Vertex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DirectedVertexTest {
+
+	Vertex uv1;
+	Vertex uv2;
+	Vertex uv3;
+	Map<Vertex, Boolean> vertices;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
