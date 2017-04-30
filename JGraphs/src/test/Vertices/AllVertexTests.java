3618030a9b8db88f631d2227a21dfe9fff03091b
venkatkorapaty <venkat.korapaty@mail.utoronto.ca>
2017-04-30 14:20:44 -0400

    changed VertexTest to UndirectedVertexTest

--- JGraphs/src/test/Vertices/AllVertexTests.java
+++ JGraphs/src/test/Vertices/AllVertexTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ VertexTest.class })
+@SuiteClasses({ UndirectedVertexTest.class })
 
 public class AllVertexTests {
 
